5a82c5874ad24d37be208e49436e7e5281299fb2
Marek <marecica2@gmail.com>
2014-11-12 11:00:58 +0100

    before datamapper update

--- src/main/java/org/mule/templates/WorkersRequest.java
+++ src/main/java/org/mule/templates/WorkersRequest.java
@@ -9,10 +9,6 @@ package org.mule.templates;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-
 import com.workday.hr.EmployeeGetType;
 import com.workday.hr.EmployeeReferenceType;
 import com.workday.hr.ExternalIntegrationIDReferenceDataType;
@@ -23,14 +19,12 @@ public class WorkersRequest {
 	public static EmployeeGetType createEmployeeRequest(String value) throws Exception{
 		final GregorianCalendar gcalendar = new GregorianCalendar();
 		gcalendar.setTime(new Date());
-		final XMLGregorianCalendar xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcalendar);
 
 		final EmployeeGetType employeeGetType = new EmployeeGetType();
 		final EmployeeReferenceType employeeReferenceType = new EmployeeReferenceType();
 		final ExternalIntegrationIDReferenceDataType extIdReference = new ExternalIntegrationIDReferenceDataType();
 		
 		final IDType idType = new IDType();
-		//idType.setSystemID("Jobvite");
 		idType.setSystemID("Salesforce - Chatter");
 		idType.setValue(value);
 		
@@ -38,18 +32,6 @@ public class WorkersRequest {
 		employeeReferenceType.setIntegrationIDReference(extIdReference);
 
 		employeeGetType.setEmployeeReference(employeeReferenceType);
-		//employeeGetType.setAsOfDate(xmlDate);
-		//employeeGetType.setAsOfMoment(xmlDate);
-		
-//		EmployeeType employeeType = new EmployeeType();
-//		employeeType.getEmployeeData().get(0).getEmployeeID();
 		return employeeGetType;
 	}
-
-	private static XMLGregorianCalendar xmlDate(Date date) throws DatatypeConfigurationException {
-		GregorianCalendar gregorianCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
-		gregorianCalendar.setTime(date);
-		return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
-	}
-	
 }
